ce44a948ebf86d2ff90fcb94577a05fe28bef037
Mathieu Moulin <mathieu@iprospective.fr>
2024-11-07 16:02:26 +0100

    Option SHIPPING_PDF_HIDE_WEIGHT_AND_VOLUME in module setup

--- admin/setup.php
+++ admin/setup.php
@@ -31,6 +31,7 @@ $arrayofparameters = array(
 	'MMI_SHIPPING'=>array('type'=>'separator','enabled'=>1),
 	'MMISHIPPING_DF'=>array('type'=>'yesno', 'enabled'=>1),
 	'MMISHIPPING_DF_ENTREPOT'=>array('type'=>'int', 'enabled'=>1),
+	'SHIPPING_PDF_HIDE_WEIGHT_AND_VOLUME'=>array('type'=>'yesno', 'enabled'=>1),
 
 	'MMI_RECEPTION'=>array('type'=>'separator','enabled'=>1),
 	'MMI_RECEPTION_SENDER_ADDRESS'=>array('type'=>'yesno', 'enabled'=>1),
